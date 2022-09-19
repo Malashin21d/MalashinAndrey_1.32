12b8f64ee9d537bf000c5b8b7bb94d1a348d501a
Malashin21d <113889092+Malashin21d@users.noreply.github.com>
2022-09-19 12:25:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+int m;
+int v;
+int p;
+Console.Write("Введите массу ");
+m = int.Parse(Console.ReadLine());
+Console.Write("Введите объём ");
+v = int.Parse(Console.ReadLine());
+p = m / v;
+Console.WriteLine("Плотность = {0}", p);
+Console.ReadLine();
